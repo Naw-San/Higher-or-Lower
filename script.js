44cd89551656aee85f8b493ec6e1491d2c674903
Naw-San <112665148+Naw-San@users.noreply.github.com>
2024-02-15 18:53:35 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const logo = document.querySelector('.logo img');
+const accountNames = document.querySelectorAll('.account-name');
+const accountDescriptions = document.querySelectorAll('.account-description');
+const accountCountries = document.querySelector
